8b1ec278b2844b16155c8d24ad34c66bbef7013c
andrewmcgov <andrew.mcgoveran@shopify.com>
2018-05-28 21:32:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A weather app made for practicing React.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Get Your Weather Here</h1>
+        </header>
+        <p className="App-intro">
+          This will become a basic weather app, made for learning React.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
