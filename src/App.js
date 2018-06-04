82791a10c1c87113f57aeae4c45e9958d082fe1d
andrewmcgov <andrew.mcgoveran@shopify.com>
2018-06-03 20:47:33 -0400

    Added main compeonents, set up current weather

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React, { Component } from "react";
 import "./App.css";
+import Hero from "./components/Hero";
+import CardBanner from "./components/CardBanner";
+//import WeatherCard from "./components/WeatherCard";
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      cityID: 6094817,
+      current: {}
+    };
+  }
   async componentDidMount() {
-    let data = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?id=6094817&APPID=${
+    // let threeHourData = await fetch(
+    //   `http://api.openweathermap.org/data/2.5/forecast?id=6094817&APPID=${
+    //     process.env.REACT_APP_WEATHER_API_KEY
+    //   }`
+    // );
+    // threeHourData = await threeHourData.json();
+    this.getWeather(6094817);
+  }
+
+  async getWeather(cityID) {
+    console.log("getWeather is running");
+    let current = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&APPID=${
         process.env.REACT_APP_WEATHER_API_KEY
-      }`
+      }&units=metric`
     );
-    data = await data.json();
-    console.log(data);
+
+    current = await current.json();
+    console.log(current);
+
+    this.setState({ current });
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Get Your Weather Here</h1>
-        </header>
-        <p className="App-intro">
-          This will become a basic weather app, made for learning React.
-        </p>
+        <div className="top-section">
+          <Hero
+            current={this.state.current}
+            getWeather={this.getWeather.bind(this)}
+          />
+          <CardBanner />
+        </div>
       </div>
     );
   }
---
+++ src/components/CardBanner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./CardBanner.css";
+
+class CardBanner extends React.Component {
+  render() {
+    return (
+      <div className="card-banner">
+        <p />
+      </div>
+    );
+  }
+}
+
+export default CardBanner;
